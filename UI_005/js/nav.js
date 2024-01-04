06cb4183c3fccf49a108cc728fe7e664a9826a6f
yangtory <wjdduscldrn@naver.com>
2024-01-04 15:51:12 +0900

    몽이네집업뎃

---
+++ UI_005/js/nav.js
@@ -0,0 +1,3 @@
+const nav_home = document.querySelector("li.nav_main.home");
+const nav_login = document.querySelector("li.nav_main.login");
+const nav_join = document.querySelector("li.nav_main.join");
