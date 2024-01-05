259d93fab4d3159d40fb458d5e03615f9d1924a6
yangtory <wjdduscldrn@naver.com>
2024-01-05 12:54:54 +0900

    주석수정

--- UI_005/js/importHTML.js
+++ UI_005/js/importHTML.js
@@ -4,10 +4,10 @@ const importHTML = (tag, file) => {
   // file 이라는 매개변수에 전달된 값을 open에 주입
   xhr.open("GET", file, true);
   xhr.onreadystatechange = () => {
-    // 파일을 읽어서 할 일들을 작성
+    // 파일을 읽어서 할 일들을 작성하는 곳
     // readyState 가 Done 이면 =  브라우저가 파일을 정상적으로 받았으면
     if (xhr.readyState === XMLHttpRequest.DONE) {
-      // text를 html로 바꿔서 tag에 입혀 그려내라
+      // text를 html로 바꿔서 tag에 입혀 그려줘
       tag.innerHTML = xhr.responseText;
     }
   };
